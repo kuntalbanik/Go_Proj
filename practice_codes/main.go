94341968b86121d33fb769f8d733579e70badd3c
Kuntal Banik <deisiduke1986@gmail.com>
2025-04-15 23:24:34 +0530

    Coding started

---
+++ practice_codes/main.go
@@ -0,0 +1,16 @@
+//	--
+//
+// Simple program that prints 'Hello World'
+// --
+
+package main
+
+import (
+	"fmt"
+	"mylearning/myutil"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	myutil.PrintMessage("This message from another file")
+}
---
+++ practice_codes/myutil/myutil.go
@@ -0,0 +1,8 @@
+// Take input as a parameter and return accordingly
+package myutil
+
+import "fmt"
+
+func PrintMessage(message string) {
+	fmt.Println(message)
+}
